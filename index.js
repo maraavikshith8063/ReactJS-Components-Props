8beebc083cda3a012a7344005c3245f5180891c9
maraavikshith8063 <maraavikshith8063@gmail.com>
2021-12-01 09:06:30 +0000

    ReactJS Component And Props

---
+++ index.js
@@ -0,0 +1,19 @@
+const Button = (props) => {
+  const { className, buttonText } = props;
+
+  return <button className={`button ${className}`}>{buttonText}</button>;
+};
+
+const element = (
+  //  Write your code here.
+  <div className="socialButton">
+    <h1>Social Buttons</h1>
+    <div className="buttons-container">
+      <Button buttonText="Like" className="like-button" />
+      <Button buttonText="Comment" className="comment-button" />
+      <Button buttonText="Share" className="share-button" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
